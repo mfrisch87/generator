65f767ee9d1fba7d27069f57918d94d71cce21b9
mfrisch87 <mikey.frisch@gmail.com>
2021-03-29 07:54:27 -0700

    added questions

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const util = require('util');
-
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username.',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter a GitHub username.");
+            }
+            return true;
+        }
 ];
 
 // function to write README file
